d1bf6b6acf860ab7f49add6b161e359a3700b452
Amanda Imbre <amanda.imbre01@estudiant.upf.edu>
2026-02-10 12:37:05 +0100

    testing finished

--- test_piped.c
+++ test_piped.c
@@ -2,5 +2,7 @@
 
 int main(void){
     run_piped("ls", "wc -l");
+    //run_piped("ls", "grep this_should_not_exist"); shouldn't have any output
+    //run_piped("yes", "head -n 5"); has to return 5 y 
     return 0;
 }
